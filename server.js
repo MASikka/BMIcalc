d0c5509a556367135be804d7da2cc20f53655b38
Marek <mareksikka@gmail.com>
2021-09-09 17:14:59 +0300

    NodeJS BMIcalc

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+app.post('/', function(req,res){
+    console.log(req.body);
+    let height=Number(req.body.height);
+    let weight=Number(req.body.weight);
+    let result = (weight/(height*height)).toFixed(2);
+    let bmi = `Your BMI is: ${result}<br>`;
+    if(result<=18.5){
+        res.send(`${bmi} Underweight.`);
+    }
+    else if(result<=24.9){
+        res.send(`${bmi} Normal weight.`);
+    }
+    else if(result<=29.9){
+        res.send(`${bmi} Overweight.`);
+    }
+    else{
+        res.send(`${bmi} Obese.`);
+    }
+});
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.");
+});
